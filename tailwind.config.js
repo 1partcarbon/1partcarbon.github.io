a43646bc100bac80246705fa467a4e5bbd3e5ac5
paulhornsey <paul@1partcarbon.co.uk>
2024-09-29 20:17:08 +0100

    Converting wp site using tailwindcss
    Concentrating on mobile width first.

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,10 @@ module.exports = {
   content: ["./public/**/*.{html,js}"],
   theme: {
     extend: {
+      backgroundImage: {
+        "bubbles-down": "url('img/wh-circles-down2@2x.png')",
+        "bubbles-up": "url('img/wh-circles-up2@2x.png')",
+      },
       backgroundColor: {
         light: "#f7f1e8",
         onepcblue: "#00a1d4",
